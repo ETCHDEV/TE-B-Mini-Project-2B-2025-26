2e113d0fa0704154d4e3de7bf27a3004948a6ee2
Muaz Shaikh <muazshaikh7861@gmail.com>
2026-04-24 21:53:46 +0530

    Delete CODE directory

--- CODE/supabase/migrations/20260331020000_add_is_curated_column.sql
+++
@@ -1,9 +0,0 @@
--- Add is_curated column to courses table
-ALTER TABLE public.courses ADD COLUMN IF NOT EXISTS is_curated BOOLEAN DEFAULT false;
-
--- Update existing curated courses to set is_curated = true
-UPDATE public.courses 
-SET is_curated = true 
-WHERE instructor = 'Team Curated' 
-OR instructor = 'Aditya Verma'
-OR url LIKE '%3Blue1Brown%';
